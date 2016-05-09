fdfb15b396dae72bd013e5b29a112feefc6ade35
lorenzo.ritter <lorenzo.ritter@gmail.com>
2016-05-09 17:23:30 +0200

    implemented functionality to convert text reviews to embedding reviews

--- build_embedding_dictionary.py
+++ build_embedding_dictionary.py
@@ -8,8 +8,7 @@ file description here
 __author__ = "Lorenzo von Ritter"
 __date__ = "2016 - 05 - 09"
 
-import numpy as np
-
+import cPickle as pkl
 categories = ['books', 'dvd', 'electronics', 'kitchen_&_housewares', 'all']
 
 filepath = '/home/lorenzo/PycharmProjects/domainadaption/sorted_data_acl/'
@@ -19,9 +18,12 @@ for category in categories:
 
     glove_dict = {}     # the dictionary is going to be of shape word -> embedding vector
 
+    # read vectors into dictionary
     with open(filepath + category + '/reviews.vectors.txt') as w2v:
         for line in w2v:
             words = line.split()
-            glove_dict[words[0]] = words[1:]    # add dictionary entry for the new word
+            glove_dict[words[0]] = map(float, words[1:])  # add dictionary entry for the new word
 
-    np.save('reviews.vector.npy', glove_dict)
+    # save dictionary
+    with open(filepath + category + '/reviews.vectors.pkl', 'wb') as dictfile:
+        pkl.dump(glove_dict, dictfile)
---
+++ embed_reviews.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+'''embed_reviews.py
+
+file description here
+'''
+
+__author__ = "Lorenzo von Ritter"
+__date__ = "2016-05-09"
+
+import numpy as np
+import cPickle as pkl
+
+categories = ['books', 'dvd', 'electronics', 'kitchen_&_housewares']
+sentiments = ['positive', 'negative']
+
+filepath = '/home/lorenzo/PycharmProjects/domainadaption/sorted_data_acl/'
+
+max_length = 200
+embedding_dim = 50
+
+def read_input(filename, w2v_dict, max_length=200, embedding_dim=200):
+    print 'Embedding %s...' %filename
+    embedded_file = []
+    #for review in open(os.path.join(dirname, fname)): # could combine the following two lines
+    with open(filename, 'r') as infile:
+        #lineindex = 0
+        for review in infile:
+            #infile_data = infile.read()
+            wordindex = 0
+            embedded_line = np.zeros((max_length, embedding_dim))
+            for word in review.split():
+                # if the maximum length of words is reached, break
+                if wordindex >= max_length:
+                    break
+                try:
+                    embedded_line[wordindex] = w2v_dict[word]
+                    wordindex += 1
+                # if a key error happens, pass to next word (maybe consider a special character for unknown words)
+                except:
+                    pass
+            #embedded_file[lineindex] = np.array([word2vec_model[word] for word in review.split()])
+            #lineindex += 1
+            embedded_file.append(embedded_line)
+            #print embedded_file
+    return np.array(embedded_file)
+
+
+for category in categories:
+    print 'category: ' + category
+
+    with open(filepath + category + '/reviews.vectors.pkl') as dictfile:
+        glove_dict = pkl.load(dictfile)
+
+    for sentiment in sentiments:
+        print '\t' + sentiment
+        infilename = filepath + category + '/reviews_' + sentiment + '.txt'
+        embedded_reviews = read_input(infilename, glove_dict, max_length=max_length, embedding_dim=embedding_dim)
+
+        outfilename = filepath + category + '/reviews_' + sentiment + '.npy'
+        np.save(outfilename, embedded_reviews)
