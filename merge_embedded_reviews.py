e9b753ac11bfc7c9713fe03fa3977fa6be85581d
lorenzo.ritter <lorenzo.ritter@gmail.com>
2016-05-09 18:26:45 +0200

    adding script to merge the embedded reviews of all categories

---
+++ merge_embedded_reviews.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+'''merge_embedded_reviews.py
+
+file description here
+'''
+
+__author__ = "Lorenzo von Ritter"
+__date__ = "2016-05-09"
+
+import numpy as np
+
+categories = ['books', 'dvd', 'electronics', 'kitchen_&_housewares']
+sentiments = ['positive', 'negative']
+
+filepath = '/home/lorenzo/PycharmProjects/domainadaption/sorted_data_acl/'
+
+
+for sentiment in sentiments:
+    print 'sentiment: ' + sentiment
+    flag = 0
+
+    for category in categories:
+        print '\tcategory: ' + category
+        file = np.load(filepath + category + '/reviews_' + sentiment + '.npy')
+        # TODO: check why electronics/reviews_nevative.npy has only 566 entries
+
+        if flag==0:
+            outfile = file
+            flag = 1
+
+        else:
+            outfile = np.concatenate((outfile, file))
+
+    np.save(filepath + 'all/reviews_' + sentiment + '.npy', outfile)
