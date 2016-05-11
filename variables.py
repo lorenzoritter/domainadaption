7ae3c911156282f8443cf04c8a8b191adc5244ba
lorenzo.ritter <lorenzo.ritter@gmail.com>
2016-05-11 14:19:53 +0200

    update variable descriptions

--- variables.py
+++ variables.py
@@ -15,9 +15,9 @@ MAX_LENGTH = 200        # maximum length of reviews (longer reviews are cut, sho
 NB_EPOCHS = 1000        # number of training epochs of the neural network
 BATCH_SIZE = 32         # batch size during training of the neural network
 LSTM_OUTPUT_SIZE = 50   # number of output neurons of the LSTM
-LSTM_DROPOUT_W = 0.5
-LSTM_DROPOUT_U = 0.1
-DROPOUT = 0.5           # dropout probability at the dropout layer
+LSTM_DROPOUT_W = 0.5    # dropout fraction at the input nodes of the LSTM
+LSTM_DROPOUT_U = 0.1    # dropout fraction at the recurrent connections of the LSTM
+DROPOUT = 0.5           # dropout fraction at the dropout layer
 
 os.environ['EMBEDDING_DIM'] = str(EMBEDDING_DIM)
 os.environ['MAX_LENGTH'] = str(MAX_LENGTH)
